fd2c9838a3a46002db4e36b0ab576bd13cc4c330
Архипова Елена <e.arhipova@3h.ru>
2021-03-01 17:10:21 +0300

    Class inheritance: replace login methods to BasePage & add new tests fot login page

--- pages/locators.py
+++ pages/locators.py
@@ -1,7 +1,11 @@
 from selenium.webdriver.common.by import By
 
-class MainPageLocators:
+# class MainPageLocators:
+#     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
@@ -12,3 +16,4 @@ class ProductPageLocators:
     MESSAGE = (By.CSS_SELECTOR, ".alertinner strong")
     NAME = (By.CSS_SELECTOR, "div.col-sm-6.product_main > h1")
     PRICE = (By.CSS_SELECTOR, ".col-sm-6 .price_color")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alertinner")
--- pages/product_page.py
+++ pages/product_page.py
@@ -18,3 +18,10 @@ class ProductPage(BasePage):
         print(book_name_in_the_basket.text)
         assert book_name.text == book_name_in_the_basket.text, "Book name is not correct!"
         assert price.text in basket_price.text, "Book price is not correct!"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
+
+    def element_should_disappear(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), "Element has not disappeared"
+
