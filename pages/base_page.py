c46a336e66bf5fcdf8ef05756b8009b713c0ddbb
Архипова Елена <e.arhipova@3h.ru>
2021-04-23 13:33:38 +0300

    prepared tests for review & added marks

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,8 +3,8 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import math
 from .locators import BasePageLocators
+import math
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
--- pages/product_page.py
+++ pages/product_page.py
@@ -11,11 +11,11 @@ class ProductPage(BasePage):
     def book_with_correct_name_and_price_should_be_in_the_basket(self):
         message = self.browser.find_elements(*ProductPageLocators.MESSAGE)
         book_name = self.browser.find_element(*ProductPageLocators.NAME)
-        print(book_name.text)
         price = self.browser.find_element(*ProductPageLocators.PRICE)
+        
         book_name_in_the_basket = message[0]
         basket_price = message[2]
-        print(book_name_in_the_basket.text)
+        
         assert book_name.text == book_name_in_the_basket.text, "Book name is not correct!"
         assert price.text in basket_price.text, "Book price is not correct!"
 
